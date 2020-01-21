a67b21364bec2fec0611595ef06e4c92daae0fc7
pubyasuoka <pubyasuoka@gmail.com>
2020-01-21 15:43:13 +0000

    first test succeed

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestNextToken(t *testing.T){
-	input := `=+(){}`
+	input := `=+(){},;`
 
 	tests := []struct{
 		expectedType    token.TokenType
@@ -24,15 +24,14 @@ func TestNextToken(t *testing.T){
 	}
 
 	l := New(input)
+	
 	for i, tt := range tests {
 		tok := l.NextToken()
-
 		if tok.Type != tt.expectedType{
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
 				i, tt.expectedType, tok.Type)
 		}
-
-		if tok.Type != tt.expectedLiteral{
+		if tok.Literal != tt.expectedLiteral{
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got =%q",
 				i, tt.expectedLiteral, tok.Literal)
 		}
